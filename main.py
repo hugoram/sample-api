c7145a0263c5a0446e78b4ca4b041dfc0d451530
hugoram <hugoram78@gmail.com>
2020-10-28 20:49:51 -0600

    Create weather api and tests.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import falcon
+import json
+import requests
+
+WEATHER_API_BASE_URL = "https://www.metaweather.com/api/location"
 
 
 class MainResource(object):
@@ -7,5 +11,46 @@ class MainResource(object):
         resp.body = "Hello World"
 
 
+class WeatherResource(object):
+    def on_get(self, req, resp, location):
+        try:
+            location_search_response = self._api_call(f"/search/?query={location}")
+            if not len(location_search_response):
+                resp.status = falcon.HTTP_404
+                resp.body = json.dumps(
+                    {"message": "The location you are looking for could not be found."}
+                )
+                return
+
+            location_id = location_search_response[0].get("woeid")
+
+            location_weather_response = self._api_call(f"/{location_id}/")
+            current_weather = location_weather_response.get("consolidated_weather")[0]
+
+            resp.status = falcon.HTTP_200
+            resp.body = json.dumps(
+                {
+                    "location": location_weather_response.get("title"),
+                    "timezone": location_weather_response.get("timezone"),
+                    "date": current_weather.get("applicable_date"),
+                    "min_temp": f"{round(current_weather.get('min_temp'))}°C",
+                    "max_temp": f"{round(current_weather.get('max_temp'))}°C",
+                    "average_temp": f"{round(current_weather.get('the_temp'))}°C",
+                }
+            )
+
+        except:
+            resp.status = falcon.HTTP_500
+            resp.body = json.dumps(
+                {
+                    "message": "There was a problem retrieving the weather. Try again later."
+                }
+            )
+
+    def _api_call(self, path):
+        return requests.get(f"{WEATHER_API_BASE_URL}{path}").json()
+
+
 app = falcon.API()
 app.add_route("/", MainResource())
+app.add_route("/weather/{location}", WeatherResource())
--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
 import falcon
+import json
 import pytest
 from falcon import testing
+from unittest.mock import patch
+
 from main import app
 
 
@@ -13,3 +16,57 @@ def test_root_path(client):
     response = client.simulate_get("/")
     assert response.status == falcon.HTTP_OK
     assert response.content.decode() == "Hello World"
+
+
+@patch("requests.get")
+def test_weather_path(get_mock, client):
+    get_mock.return_value.json.side_effect = [
+        [{"woeid": "some_id"}],
+        {
+            "title": "Mexico City",
+            "timezone": "America/Mexico_City",
+            "consolidated_weather": [
+                {
+                    "min_temp": 16,
+                    "max_temp": 25,
+                    "the_temp": 20,
+                    "applicable_date": "2020-10-29",
+                }
+            ],
+        },
+    ]
+
+    response = client.simulate_get("/weather/mex")
+    assert response.status == falcon.HTTP_OK
+    assert response.content.decode() == json.dumps(
+        {
+            "location": "Mexico City",
+            "timezone": "America/Mexico_City",
+            "date": "2020-10-29",
+            "min_temp": "16°C",
+            "max_temp": "25°C",
+            "average_temp": "20°C",
+        }
+    )
+
+
+@patch("requests.get")
+def test_weather_path_with_not_found_result(get_mock, client):
+    get_mock.return_value.json.return_value = []
+
+    response = client.simulate_get("/weather/somethingbad")
+    assert response.status == falcon.HTTP_404
+    assert response.content.decode() == json.dumps(
+        {"message": "The location you are looking for could not be found."}
+    )
+
+
+@patch("requests.get")
+def test_weather_path_handles_exception(get_mock, client):
+    get_mock.return_value.json.return_value = Exception()
+
+    response = client.simulate_get("/weather/somethingverybad")
+    assert response.status == falcon.HTTP_500
+    assert response.content.decode() == json.dumps(
+        {"message": "There was a problem retrieving the weather. Try again later."}
+    )
